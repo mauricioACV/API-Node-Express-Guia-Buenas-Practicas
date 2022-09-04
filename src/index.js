bf4b0cd73b8954decd3cc96f993047b65a5c31fd
mauricioACV <mauricio.campusano@gmail.com>
2022-09-03 20:03:53 -0400

    Estrcutura base API

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hola Mundo!</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
