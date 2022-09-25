f5e863c469c1d6e552efaa7195d4dc38082e0673
mauricioACV <mauricio.campusano@gmail.com>
2022-09-25 19:52:23 -0300

    Implemento documentación por medio de Swagger y estandar openapi 3.0; Docs muestra parametros para filtro de resultados, ejemplo respuesta 200 ok y 500 Failed; Muestra schema del recurso asociado a endpoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: v1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
-const cache = apicache.middleware;
-
 const PORT = process.env.PORT || 3000;
+const cache = apicache.middleware;
 
 app.use(express.json());
 app.use(cache("3 minutes"));
@@ -13,4 +13,5 @@ app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  v1SwaggerDocs(app, PORT);
 });
